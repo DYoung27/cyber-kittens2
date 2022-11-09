1c277d8d2b0ae6b925a3d7e407fd1aff20a81878
Dyoung27 <DilipYoung27@gmail.com>
2022-11-09 14:37:37 +0000

    fixed table relations

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ const {User} = require('./User');
 const {sequelize, Sequelize} = require('./db');
 
 Kitten.belongsTo(User, {foreignKey: 'ownerId'}); // Kitten table, there will be an ownerId <- FK
-User.hasMany(Kitten);
+User.hasMany(Kitten, {foreignKey: 'ownerId'});
 
 module.exports = {
     Kitten,
